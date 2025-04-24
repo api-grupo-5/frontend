85db72115f42178d9e9165a177d6448d1939fe92
tobia-s <tobiasvlarramendi@gmail.com>
2025-04-24 18:06:31 -0300

    test:

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,9 @@ import Footer from "./components/Footer";
 export default function Home(){
     return (
         <>
+            <h1 style={{ textAlign: "center", margin: "20px 0" }}>
+                TEST
+            </h1>
             <Carousel/>
             <Footer/>
         </>
