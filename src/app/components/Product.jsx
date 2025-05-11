b93829115e57e79537389f52be4154cf3be5adb8
Francisco Solé <franciscosoleescande@gmail.com>
2025-05-11 13:16:09 -0300

    agregado los botones de editar o elimnar en el carousel

--- src/app/components/Product.jsx
+++ src/app/components/Product.jsx
@@ -1,18 +1,31 @@
 import styles from "../css/carousel.module.css";
+import { useAuth } from "../context/LoginManagement";
 
-export default function Product({ title, image, price, onHoverStart, onHoverEnd, onAddToCart }) {
+export default function Product({ title, image, price, seller, onHoverStart, onHoverEnd, onAddToCart, onClick }) {
+    const { user } = useAuth()
+
     return (
         <div
             className={styles.product_card}
             onMouseEnter={onHoverStart}
             onMouseLeave={onHoverEnd}
         >
-            <img src={image} alt={title} className={styles.product_image} />
-            <h3 className={styles.product_title}>{title}</h3>
-            <p className={styles.product_price}>${price}</p>
-            <button className={styles.add_to_cart_btn} onClick={onAddToCart}>
-                Agregar al carrito
-            </button>
+            <div onClick={onClick}>
+                <img src={image} alt={title} className={styles.product_image} />
+                <h3 className={styles.product_title}>{title}</h3>
+                <p className={styles.product_price}>${price}</p>
+            </div>
+
+            {user && seller == user.email ? (
+                <div className={styles.botonesAdmin}>
+                    <button className={styles.btnEditar}>Editar</button>
+                    <button className={styles.btnEliminar}>Eliminar</button>
+                </div>
+                ) : 
+                <button className={styles.add_to_cart_btn} onClick={onAddToCart}>
+                    Agregar al carrito
+                </button>
+            }
         </div>
     );
 }
