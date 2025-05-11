7a64283f47b4b005cd188c698025dcb747e72e34
Francisco Solé <franciscosoleescande@gmail.com>
2025-05-11 13:18:13 -0300

    agregado sistema de busqueda de articulos

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,33 @@
 'use client'
-
 import Link from 'next/link'
 import Image from 'next/image';
 import styles from '../css/navbar.module.css'
 import { FaUser, FaShoppingCart, FaSearch } from 'react-icons/fa'
 import { redirect } from "next/navigation";
+import { products } from '../data/products';
+import { useState, useEffect } from 'react';
+
 
 export default function Navbar() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  const allProducts = Object.values(products).flatMap(cat => Object.values(cat));
+
+  useEffect(() => {
+    if (search.trim() === '') {
+      setResults([]);
+      return;
+    }
+
+    const filtered = allProducts.filter(product =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setResults(filtered);
+  }, [search]);
+
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
@@ -25,9 +46,26 @@ export default function Navbar() {
         {/* Buscador */}
         <div className={styles.center}>
           <div className={styles.searchBox}>
-            <input type="text" placeholder="Buscar productos..." />
+            <input
+              type="text"
+              placeholder="Buscar productos..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
             <FaSearch className={styles.searchIcon} />
           </div>
+
+          {results.length > 0 && (
+            <ul className={styles.searchResults}>
+              {results.map(product => (
+                <li key={product.id}>
+                  <Link href={`../producto/${product.id}`}>
+                    {product.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
 
         {/* Iconos */}
@@ -51,7 +89,7 @@ export default function Navbar() {
             <li><Link href="/categoria/computacion">Computación</Link></li>
             <li><Link href="/categoria/electrodomesticos">Electrodomésticos</Link></li>
             <li><Link href="/categoria/perifericos">Periféricos</Link></li>
-            <li><Link href="/emular-venta">Cargar producto</Link></li>
+            <li><Link href="/emular-venta">Vender</Link></li>
           </ul>
         </div>
       </div>
